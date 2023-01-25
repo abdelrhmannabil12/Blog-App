5400e6a1ddf70cdb69a5deecefa3aaa07af94282
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2023-01-25 17:33:37 +0200

    created the blog model and register it

--- accounts/models.py
+++ accounts/models.py
@@ -44,7 +44,7 @@ class Account(AbstractBaseUser):
     last_name=models.CharField(max_length=50)
     username=models.CharField(max_length=50,unique=True)
     email=models.EmailField(max_length=50,unique=True)
-    profile_picture=models.ImageField(blank=True,upload_to='userprofile')
+    profile_picture=models.ImageField(blank=True,upload_to='images/userprofile')
     date_joined=models.DateTimeField(auto_now_add=True)
     last_login=models.DateTimeField(auto_now_add=True)
     is_admin=models.BooleanField(default=False)
--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from accounts.models import *
 # Create your models here.
+class Blog(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    title=models.CharField(max_length=50)
+    img=models.ImageField(upload_to=f'images/blog/')
+    description=models.TextField(unique=True)
+    created_date    = models.DateTimeField(auto_now_add=True)
+    modified_date   = models.DateTimeField(auto_now=True)
