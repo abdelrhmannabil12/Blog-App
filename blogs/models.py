4b7c1192ae936c9e08e0b0f6ce81f4f8a775fbb2
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2023-01-26 23:05:39 +0200

    created the category model

--- blogs/models.py
+++ blogs/models.py
@@ -2,13 +2,29 @@ from django.db import models
 from django.urls import reverse
 from accounts.models import *
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50,blank=True,null=True, unique=True)
+    slug = models.SlugField(max_length=100, blank=True,null=True,unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    cat_image = models.ImageField(upload_to='Images/categories', blank=True)
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_url(self):
+            return reverse('blogs_by_category', args=[self.slug])
+
+    def __str__(self):
+        return self.category_name
 class Blog(models.Model):
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
     title=models.CharField(max_length=50)
     img=models.ImageField(upload_to=f'images/blog/')
     description=models.TextField(unique=True)
+    category        = models.ForeignKey(Category,blank=True,null=True, on_delete=models.CASCADE)
     created_date    = models.DateTimeField(auto_now_add=True)
     modified_date   = models.DateTimeField(auto_now=True) 
 
     def get_url(self):
         return reverse('blog_details', kwargs={"id":self.id})
+
