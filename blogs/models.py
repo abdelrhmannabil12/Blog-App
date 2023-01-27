4a0e0cfc745b676bbc8097b7f55d25717cc8655e
abdelrhmannabil12 <59992853+abdelrhmannabil12@users.noreply.github.com>
2023-01-27 15:38:36 +0200

    add the edit and delete urls

--- blogs/models.py
+++ blogs/models.py
@@ -6,20 +6,21 @@ class Category(models.Model):
     category_name = models.CharField(max_length=50,blank=True,null=True, unique=True)
     slug = models.SlugField(max_length=100, blank=True,null=True,unique=True)
     description = models.TextField(max_length=255, blank=True)
-    cat_image = models.ImageField(upload_to='Images/categories', blank=True)
+    cat_image = models.ImageField(upload_to='images/categories', blank=True)
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
     def get_url(self):
             return reverse('blogs_by_category', args=[self.slug])
+    
 
     def __str__(self):
         return self.category_name
 class Blog(models.Model):
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
     title=models.CharField(max_length=50)
-    img=models.ImageField(upload_to=f'images/blog/')
+    img=models.ImageField(upload_to='images/blog/')
     description=models.TextField(unique=True)
     category        = models.ForeignKey(Category,blank=True,null=True, on_delete=models.CASCADE)
     created_date    = models.DateTimeField(auto_now_add=True)
@@ -27,4 +28,8 @@ class Blog(models.Model):
 
     def get_url(self):
         return reverse('blog_details', kwargs={"id":self.id})
-
+    def edit_blog_url(self):
+        return reverse('edit_blog', kwargs={"id":self.id})    
+    
+    def delete_url(self):
+        return reverse('delete_blog',kwargs={"id":self.id})
